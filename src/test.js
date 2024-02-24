2f14e25218752939c029b90d46ff6f492092ad81
Labidi-Ahmed <jackitunisi@gmail.com>
2024-02-24 18:02:36 +0100

    init commit

---
+++ src/test.js
@@ -0,0 +1,7 @@
+let m;
+$;
+const k = 'hhhhhhh';
+
+function amed() {
+	console.log(';ol');
+}
---
+++ webpack.prod.js
@@ -0,0 +1,84 @@
+const { Module } = require('module');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin') ;
+
+module.exports = {
+    mode: 'production',
+    entry:{ 
+        bundle: path.resolve(__dirname, 'src/index.js')
+},
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devServer : {
+        static : {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+       
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+
+
+    },
+    devtool: 'source-map',
+
+
+    module : {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                
+              
+              },
+              {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+              },
+              {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+              },
+
+
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template:'src/index.html',
+        },),
+    
+
+    ],
+}
+
